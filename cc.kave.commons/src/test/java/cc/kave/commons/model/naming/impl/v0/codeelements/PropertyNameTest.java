8f77f058da19314db91376c24f48282ebcb5b732
Sebastian Proksch <development@mail.proks.ch>
2018-04-18 05:07:59 +0200

    Add forgotten tests for the PropertyName extension.

--- cc.kave.commons/src/test/java/cc/kave/commons/model/naming/impl/v0/codeelements/PropertyNameTest.java
+++ cc.kave.commons/src/test/java/cc/kave/commons/model/naming/impl/v0/codeelements/PropertyNameTest.java
@@ -15,10 +15,24 @@
  */
 package cc.kave.commons.model.naming.impl.v0.codeelements;
 
+import static cc.kave.commons.model.naming.Names.newMethod;
+import static cc.kave.commons.model.naming.Names.newProperty;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.google.common.collect.Lists;
+
+import cc.kave.commons.exceptions.AssertionException;
 import cc.kave.commons.exceptions.ValidationException;
+import cc.kave.commons.model.naming.Names;
+import cc.kave.commons.model.naming.codeelements.IMethodName;
+import cc.kave.commons.model.naming.codeelements.IParameterName;
+import cc.kave.commons.model.naming.codeelements.IPropertyName;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
@@ -38,8 +52,112 @@ public class PropertyNameTest {
 			"get set [?] [?].P", // no paranthesis
 			"[?] [?].P()" // neither get nor set
 	})
+
 	public void ShouldRejectInvalidProperties(String invalidId) {
 		// ReSharper disable once ObjectCreationAsStatement
 		new PropertyName(invalidId);
+	}
+
+	@Test
+	public void getExplicitGetter_regular() {
+		IMethodName actual = Names.newProperty("set get [p:int] [p:object].P()").getExplicitGetterName();
+		IMethodName expected = Names.newMethod("[p:int] [p:object].P__get__()");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getExplicitGetter_indexer() {
+		IMethodName actual = Names.newProperty("set get [p:int] [p:object].P([p:bool] p)").getExplicitGetterName();
+		IMethodName expected = Names.newMethod("[p:int] [p:object].P__get__([p:bool] p)");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getExplicitSetter_regular() {
+		IMethodName actual = newProperty("set get [p:int] [p:object].P()").getExplicitSetterName();
+		IMethodName expected = newMethod("[p:void] [p:object].P__set__([p:int] value)");
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getExplicitSetter_indexer() {
+		IMethodName actual = newProperty("set get [p:int] [p:object].P([p:bool] p)").getExplicitSetterName();
+		IMethodName expected = newMethod("[p:void] [p:object].P__set__([p:bool] p, [p:int] value)");
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected = AssertionException.class)
+	public void getExplicitGetter_notImplemented() {
+		newProperty("set [p:int] [p:object].P()").getExplicitGetterName();
+	}
+
+	@Test(expected = AssertionException.class)
+	public void getExplicitSetter_notImplemented() {
+		newProperty("get [p:int] [p:object].P()").getExplicitSetterName();
+	}
+
+	@Test
+	public void canRequestParametersFromIndexer() {
+		List<IParameterName> actuals = newProperty("set get [p:int] [p:object].P([p:bool] idx)").getParameters();
+		List<IParameterName> expecteds = Lists.newArrayList(Names.newParameter("[p:bool] idx"));
+		assertEquals(expecteds, actuals);
+	}
+
+	@Test(expected = AssertionException.class)
+	public void cannotRequestParametersFromNonIndexer() {
+		Names.newProperty("set get [p:int] [p:object].P()").getParameters();
+
+	}
+
+	@Test
+	public void getExplicitGetterIsStable() {
+		IPropertyName n = Names.newProperty("set get [p:int] [p:object].P()");
+		IMethodName a = n.getExplicitGetterName();
+		IMethodName b = n.getExplicitGetterName();
+		assertSame(a, b);
+	}
+
+	@Test
+	public void getExplicitSetterIsStable() {
+		IPropertyName n = Names.newProperty("set get [p:int] [p:object].P()");
+		IMethodName a = n.getExplicitSetterName();
+		IMethodName b = n.getExplicitSetterName();
+		assertSame(a, b);
+	}
+
+	@Test
+	public void getExplicitSetterParametersAreStable() {
+		IPropertyName n = Names.newProperty("set get [p:int] [p:object].P()");
+		List<IParameterName> pA = n.getExplicitSetterName().getParameters();
+		List<IParameterName> pB = n.getExplicitSetterName().getParameters();
+		assertSame(pA, pB);
+		assertSame(pA.get(0), pB.get(0));
+	}
+
+	@Test
+	public void getExplicitGetterParametersAreStable() {
+		IPropertyName n = Names.newProperty("set get [p:int] [p:object].P([p:bool] p)");
+		List<IParameterName> pA = n.getExplicitSetterName().getParameters();
+		List<IParameterName> pB = n.getExplicitSetterName().getParameters();
+		assertSame(pA, pB);
+		assertSame(pA.get(0), pB.get(0));
+	}
+
+	@Test
+	public void explicitParameterNamesAreStable_indexerSet() {
+		IPropertyName n = Names.newProperty("set [p:int] [p:object].P([p:bool] p)");
+		List<IParameterName> pA = n.getParameters();
+		List<IParameterName> pB = n.getExplicitSetterName().getParameters();
+		assertEquals(pA.size(), pB.size() - 1);
+		assertSame(pA.get(0), pB.get(0));
+	}
+
+	@Test
+	public void explicitParameterNamesAreStable_indexerGet() {
+		IPropertyName n = Names.newProperty("get [p:int] [p:object].P([p:bool] p)");
+		List<IParameterName> pA = n.getParameters();
+		List<IParameterName> pB = n.getExplicitGetterName().getParameters();
+		assertSame(pA, pB);
+		assertSame(pA.get(0), pB.get(0));
 	}
 }
